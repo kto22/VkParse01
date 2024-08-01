d0ee257b81b365e6a743c7a8713ba249ea9d730d
KonaTyan <hesoyam223377@gmail.com>
2024-08-01 10:41:39 +0500

    added getMessageExternalId func

--- VkApiFunc.py
+++ VkApiFunc.py
@@ -30,19 +30,26 @@ def getMessageCount(session_api, user_id: int) -> int:
     return data['count']
 
 
-def getMessageExternalId(session_api, user_id: int, internal_id: int, start_message_id: int) -> int:
+def getMessageExternalId(session_api, user_id: int, internal_id: int) -> int:
 
-    for i in range(getMessageCount(session_api, user_id)//200+1):
+    start_message_id = getLastMessage(session_api, user_id)['id']
+    count = internal_id
+
+    for c in range(getMessageCount(session_api, user_id)//200+1):
 
         history = (session_api.messages.getHistory(
-            count=200 if internal_id > 199 else internal_id,
+            count=200 if count > 199 else count,
             user_id=user_id,
             start_message_id=start_message_id
         ))
-        internal_id -= 199
+        count -= 199
         start_message_id = history['items'][-1]['id']
         history['items'].pop(-1)
-        # это когда-нибудь потом доделать
+
+        for i in history['items']:
+            if i['conversation_message_id']==internal_id:
+                return i['id']
+    return 0
 
 
 def Parse(session_api, user_id: int, start_message_id: int, count: int) -> None:
@@ -53,7 +60,6 @@ def Parse(session_api, user_id: int, start_message_id: int, count: int) -> None:
         writer.writerow(['user', 'message', 'in_msg_id'])
 
         message_count = getMessageCount(session_api, user_id)
-
         if message_count < count:
             count = message_count+1
 
@@ -88,7 +94,7 @@ def Parse(session_api, user_id: int, start_message_id: int, count: int) -> None:
     print("Done!")
 
 
-
+# -------------------------TESTS--------------------------------------------------
 
 token = 'vk1.a.6kRn17RcZyO5gIVW9sVx8bjPr5nU1P7G2kEin1T0Y8u3fJVmQmq4xGwUnZIoQmJwLuQQDYT4YzzKeb7vuoaBBE-LRSAeY8PL0TPp18SSbM39eErT6SY3yZO6-aPBTZjn_5WU7u-T3d4KnxcuVW0PJbWYI9xgiz0NSry8eG6ZUeuJRaHpUgzwpaZpvfld2bcInc8cBLzEfSI5L8Ur_DIzKA'
 
@@ -98,8 +104,11 @@ session_api = vk_session.get_api()
     #381832378
     #464241833
 
-user_id = 2000000096
-Parse(session_api, user_id, getLastMessage(session_api, user_id)['id'], 1000000000)
+user_id = 381832378
+
+a = getMessageExternalId(session_api, user_id, 196)
+print(a)
+#Parse(session_api, user_id, getLastMessage(session_api, user_id)['id'], 1000000000)
 
 
 

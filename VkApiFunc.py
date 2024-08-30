9cf42531c21fe157d5838e4c590d7ccd74657576
KonaTyan <hesoyam223377@gmail.com>
2024-08-30 17:46:30 +0500

    deleted test files

--- VkApiFunc.py
+++ VkApiFunc.py
@@ -1,80 +1,177 @@
 
+import multiprocessing
 import vk_api
 import csv
+import os
+from CSVFunctions import repeatDelete, repeatCount
 
 
-def getFirstMessage(session_api, user_id: int) -> int:
-    data = (session_api.messages.getHistory(
-        count=1,
-        user_id=user_id,
-        rev=1
-    ))
-    return data['items'][0]
+class VkParser:
+    __slots__ = ("token", "user_id", "session_api")
+
+    def __init__(self, token: str, user_id: int) -> None:
+        self.user_id = user_id
+        self.token = token
+        vk_session = vk_api.VkApi(token=token)
+        session_api = vk_session.get_api()
+        self.session_api = session_api
+
+    def getFirstMessage(self) -> int:
+        data = (self.session_api.messages.getHistory(
+            count=1,
+            user_id=self.user_id,
+            rev=1
+        ))
+        return data['items'][0]
+
+    def getLastMessage(self) -> int:
+        data = (self.session_api.messages.getHistory(
+            count=1,
+            user_id=self.user_id,
+            rev=0
+        ))
+        return data['items'][0]
+
+    def getMessageCount(self) -> int:
+        data = (self.session_api.messages.getHistory(
+            count=0,
+            user_id=self.user_id
+        ))
+        return data['count']
+
+    def getMessageExternalId(self, start_message_internal_id: int) -> int:
+        data = self.session_api.messages.getByConversationMessageId(
+            peer_id=self.user_id,
+            conversation_message_ids=start_message_internal_id
+        )
+        return data['items'][0]['id']
 
 
-def getLastMessage(session_api, user_id: int) -> int:
-    data = (session_api.messages.getHistory(
-        count=1,
-        user_id=user_id,
-        rev=0
-    ))
-    return data['items'][0]
 
+    def Parse(self, start_message_internal_id: int, count: int) -> None:
 
-def getMessageCount(session_api, user_id: int) -> int:
-    data = (session_api.messages.getHistory(
-        count=0,
-        user_id=user_id
-    ))
-    return data['count']
-
-
-def getMessageByInternalId(session_api, user_id: int, internal_id: int) -> int:
-
-    data = session_api.messages.getByConversationMessageId(
-        peer_id=user_id,
-        conversation_message_ids=internal_id
-    )
-    return data['items'][0]
-
-
-def Parse(session_api, user_id: int, start_message_id: int, count: int) -> None:
-
-    with open(f"CSV/{user_id}_file.csv", "w", newline="", encoding="utf-8") as csvfile:
-        writer = csv.writer(csvfile, delimiter=";")
-        writer.writerow(['user', 'message', 'in_msg_id'])
-
-        message_count = getMessageCount(session_api, user_id)
+        message_count = self.getMessageCount()
         if message_count < count:
-            count = message_count+1
+            count = message_count + 1
+        for process_id in range(count // 200 + 1):
 
-        for c in range(count//199+1):
+
+            ParseProcess(process_id, self.token, self.user_id, start_message_internal_id).start()
+
+            """multiprocessing.Process(target=getMessages, args=(self.token,
+                                                              self.user_id,
+                                                              process_id,
+                                                              start_message_internal_id)).start()"""
+
+            print(f'the process {process_id} has started its work!')
+            count -= 200
+            start_message_internal_id -= 200
+
+        print("All processes are running!")
+
+
+'''
+def getMessages(token, user_id, process_id, start_message_internal_id) -> None:
+    vk_session = vk_api.VkApi(token=token)
+    session_api = vk_session.get_api()
+
+    if not os.path.isdir('CSV_temp'):
+        os.mkdir('CSV_temp')
+
+    with open(f"CSV_temp/{process_id}_file.csv", "w", newline="", encoding="utf-8") as csvfile:
+        writer = csv.writer(csvfile, delimiter=";")
+        iter_count = 200
+        while True:
+            try:
+                data = session_api.messages.getByConversationMessageId(
+                    peer_id=user_id,
+                    conversation_message_ids=start_message_internal_id
+                )
+                message_id = data['items'][0]['id']
+                break
+            except:
+                start_message_internal_id -= 1
+                iter_count-=1
+
+        history = (session_api.messages.getHistory(
+            count=iter_count,
+            user_id=user_id,
+            start_message_id=message_id
+        ))
+
+        for c in range(200):
+            i = history['items'][c]
+            message = ''.join(i['text'].splitlines())
+            id = i['conversation_message_id']
+            user = i['from_id']
+
+            writer.writerow([user, message, id])
+
+    print(f'the process {process_id} has completed its work!')
+'''
+
+
+class ParseProcess(multiprocessing.Process):
+    __slots__ = ("process_id", "token", "user_id", "start_message_internal_id")
+    def __init__(self, process_id: int, token: str, user_id: int, start_message_internal_id: int):
+        super().__init__()
+        self.process_id = process_id
+        self.token = token
+        self.user_id = user_id
+        self.start_message_internal_id = start_message_internal_id
+
+    def run(self) -> None:
+        vk_session = vk_api.VkApi(token=self.token)
+        session_api = vk_session.get_api()
+
+        if not os.path.isdir('CSV_temp'):
+            os.mkdir('CSV_temp')
+
+        with open(f"CSV_temp/{self.process_id}_file.csv", "w", newline="", encoding="utf-8") as csvfile:
+            writer = csv.writer(csvfile, delimiter=";")
+            iter_count = 200
+            while True:
+                try:
+                    data = session_api.messages.getByConversationMessageId(
+                        peer_id=self.user_id,
+                        conversation_message_ids=self.start_message_internal_id
+                    )
+                    message_id = data['items'][0]['id']
+                    break
+                except:
+                    self.start_message_internal_id -= 1
+                    iter_count -= 1
+
+                    print(f'bebra{self.process_id}-----------------------------------------------------------')
 
             history = (session_api.messages.getHistory(
-                count=200 if count > 199 else count,
-                user_id=user_id,
-                start_message_id=start_message_id
+                count=iter_count,
+                user_id=self.user_id,
+                start_message_id=message_id
             ))
 
-            count -= 199
-            start_message_id = history['items'][-1]['id']
-
-            last_item = history['items'][-1]
-            history['items'].pop(-1)
-
-
-            for i in history['items']:
-
-                message = i['text']
+            for c in range(iter_count):
+                i = history['items'][c]
+                message = ''.join(i['text'].splitlines())
                 id = i['conversation_message_id']
                 user = i['from_id']
-
+                if id <= self.start_message_internal_id-200:
+                    break
                 writer.writerow([user, message, id])
 
-        message = last_item['text']
-        id = last_item['conversation_message_id']
-        user = last_item['from_id']
-        writer.writerow([user, message, id])
+        repeatDelete(self.process_id, 200-iter_count)
+        print(f'the process {self.process_id} has completed its work!')
 
-    print("Done") 
 
+# -------------------------TESTS--------------------------------------------------
+
+if __name__ == '__main__':
+
+    token = ''
+
+
+    user_id = 263703091
+
+    parse_vk = VkParser(token, user_id)
+    parse_vk.Parse(61000, 59999)
+    print(parse_vk.getMessageCount())
